2ace89802f51ea5fe438d8624f9bd6d2aa5cad18
Carsten König <Carsten@gettingsharper.de>
2015-09-29 08:17:11 +0200

    Formelparser fertiggestellt

--- src/CountdownGame/Parser.hs
+++ src/CountdownGame/Parser.hs
@@ -15,7 +15,28 @@ tryParse = tryParseWith expressionP
 type Parser = Parsec Text ()
 
 expressionP :: Parser Expression
-expressionP = Value . read <$> many1 digit
+expressionP = addP
+
+addP :: Parser Expression
+addP = chainl1 multP addOp
+  where addOp = choice [plusP, subP]
+        plusP = (char '+' *> (return $ Apply Add))
+        subP  = (char '-' *> (return $ Apply Sub))
+
+multP :: Parser Expression
+multP = chainl1 valueP multOp
+  where multOp = choice [mulP,divP]
+        mulP = (char '*' *> (return $ Apply Mul))
+        divP = (char '/' *> (return $ Apply Div))
+
+valueP :: Parser Expression
+valueP = spaces *> (bracesP <|> numberP) <* spaces
+
+bracesP :: Parser Expression
+bracesP = between (char '(') (char ')') expressionP
+
+numberP :: Parser Expression
+numberP = Value . read <$> many1 digit
 
 tryParseWith :: Parser a -> Text -> Maybe a
 tryParseWith p txt =
